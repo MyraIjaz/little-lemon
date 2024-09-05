38f41cb9ffce1b7e14635577fa4e0e857c41aa0f
Myra Ijaz <myraijaz9@gmail.com>
2024-09-05 10:50:14 +0500

    changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import {FaBiking} from "react-icons/fa"
 const Main = () => {
   return (
     <div className="container-main">
-<main>
+<main id="menu">
 <ul className="main-list">
   <li className="main-item">This week's Special</li>
   <li ><button className="main-button"><span>OnlineMenu</span></button></li>
@@ -19,8 +19,7 @@ const Main = () => {
     <li className="menu-price">$20.00</li>
   </ul>
     <p className="para">
-    Greek salad with crisp lettuce, fresh peppers, olives, 
-    and Chicago-style feta cheese, garnished with crunchy garlic .</p>
+    Crisp lettuce, fresh peppers, olives, and Chicago-style feta cheese, all garnished with crunchy garlic, make a refreshing Greek salad.</p>
   <ul className="menu-delivery">
     <li>
     <button className="card-btn">Order & Delivery</button>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,30 +2,42 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../App.css"
 import Logo from "../icons_assets/Logo.svg"
+import { useState } from 'react'
+import {FaBars} from "react-icons/fa"
 
 
 const Nav = () => {
+  const[isOpen, setIsOpen] =useState(false);
+  const toggleMenu=()=>{
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div className="container">
-       <nav>
-        <ul className="nav-list">
-                <li className="nav-sub">  <img src={Logo} alt="logo"/></li>          
-  <li className="nav-sub"> <Link to="/">Home</Link></li>
+
+       <nav className="dvs-header">
+        <ul className="d-flex wrap justify-between align-center">
+                <li>  <img src={Logo} alt="logo"/></li> 
+                <li className="dvs-header__trigger" onClick={toggleMenu}><FaBars/></li>
+                
+               <li className={`dvs-header__menuItems ${isOpen ? "is-open":""} `}>
+                <ul>
+                <li className="nav-sub"> <Link to="/">Home</Link></li>
  
-  <li className="nav-sub"> <Link to="/about">About</Link></li>
- 
-
-    <li className="nav-sub"> <Link to="/menu">Menu</Link></li>
-   
-     <li className="nav-sub"> <Link to="/reservations">Reservations</Link></li>
-   
- <li className="nav-sub"> <Link to="/orderonline">OrderOline</Link></li>
-   <li className="nav-sub"> <Link to="/login">Login</Link></li>
- </ul>
+ <li className="nav-sub"> <Link to="/about">About</Link></li>
 
 
+   <li className="nav-sub"> <a href="#menu">Menu</a></li>
+  
+    <li className="nav-sub"> <Link to="/reservations">Reservations</Link></li>
+  
+<li className="nav-sub"> <Link to="/orderonline">OrderOline</Link></li>
+  <li className="nav-sub"> <Link to="/login">Login</Link></li>
+               </ul>
+               </li>
 
-    </nav>
+                 </ul>
+</nav>
     </div>
    
   )
